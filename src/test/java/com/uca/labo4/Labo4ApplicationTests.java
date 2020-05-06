3ef6338e067485bc99207833eca476c30cbbd22d
NitroSkate <00159817@uca.edu.sv>
2020-05-06 15:34:51 -0600

    terminado

---
+++ src/test/java/com/uca/labo4/Labo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.labo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
